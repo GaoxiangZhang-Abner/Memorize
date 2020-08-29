a139126c98e6cc64904c10225ff2d5e0e04a504b
Gaoxiang Zhang <ios4zgx@163.com>
2020-08-29 08:55:06 +0800

    Basic introduction to SwiftUI

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,9 +8,16 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+// keyword is megenta (洋红)
+// name of identifier is green
+// Swift's identifier is purple
+// ContentView behavies like a view (not 'is a view') because it is not OOP, it's functional programming
+struct ContentView: View { // curly brace (花括号)
+    
+    // some mean the variable body is any type, any struct which behavies like a View
+    // 即body不论什么type，只要它表现得像个View
+    var body: some View { // 此处var的value没有存储在内存中，而是在向var取值时计算curly brace中的内容并return
+        return Text("Hello, World!") // in parentheses 括号中
     }
 }
 
