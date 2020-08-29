e85eb46fe2e817caab2768bba090dca580d7938c
Gaoxiang Zhang <ios4zgx@163.com>
2020-08-29 12:16:36 +0800

    CardView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -6,18 +6,47 @@
 //  Copyright © 2020 Gaoxiang Zhang. All rights reserved.
 //
 
-import SwiftUI
-
 // keyword is megenta (洋红)
 // name of identifier is green
 // Swift's identifier is purple
 // ContentView behavies like a view (not 'is a view') because it is not OOP, it's functional programming
-struct ContentView: View { // curly brace (花括号)
+// { } curly brace (花括号)
+
+// some mean the variable body is any type, any struct which behavies like a View
+// 即body不论什么type，只要它表现得像个View
+// 此处var的value没有存储在内存中，而是在向var取值时计算curly brace中的内容并return
+// var body: some View {
+//      Text("Hello, World!") // in parentheses 括号中
+// }
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            ForEach(0..<3) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
     
-    // some mean the variable body is any type, any struct which behavies like a View
-    // 即body不论什么type，只要它表现得像个View
-    var body: some View { // 此处var的value没有存储在内存中，而是在向var取值时计算curly brace中的内容并return
-        return Text("Hello, World!") // in parentheses 括号中
+    var body: some View {
+        ZStack {
+            if (isFaceUp) {
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                Text("🐶")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
 
