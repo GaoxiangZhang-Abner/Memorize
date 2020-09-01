2f47b12e93d39e215c36c86192da1722459b7482
Gaoxiang Zhang <ios4zgx@163.com>
2020-09-01 23:09:06 +0800

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Gaoxiang Zhang on 2020/9/1.
+//  Copyright © 2020 Gaoxiang Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
